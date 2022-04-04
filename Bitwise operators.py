3af978202ba80a77055669146d136f260e65289e
Teddybrian <77846639+Teddybrian@users.noreply.github.com>
2022-04-04 11:16:01 +0300

    Add files via upload

---
+++ Bitwise operators.py
@@ -0,0 +1,22 @@
+#Bitwise operators
+a = 20
+b = 10
+
+#Bitwise AND operator
+print(a & b)
+
+#Bitwise OR operator
+print(a | b)
+
+#Bitwise NOT operator
+print(~a)
+print(~b)
+
+#Bitwise XOR operator
+print(a ^ b)
+
+#Bitwise RIGHT SHIFT operator
+print(a >> 2)
+
+#Bitwise LEFT SHIFT operator
+print(a << 2)
